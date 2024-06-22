350b7d33df29e2a9699a695f36fc6a52a3fc838c
Koichiro Mikama <koichiro.mika@gmail.com>
2024-06-22 23:04:57 +0900

    Add About page

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,13 +1,21 @@
-require "test_helper"
+require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
-    get static_pages_home_url
+  test 'should get home' do # 「homeページのテスト」
+    get static_pages_home_url # static_pages/home のURLにHTTP GETリクエストを送信します。
+    # このリクエストは、static_pagesコントローラのhomeアクションを呼び出します。
+    assert_response :success # その結果のレスポンスは成功（例: HTTPステータスコード200）であるべきです。
+  end
+
+  test 'should get help' do
+    # "static_pages_help_url"はテスト用のヘルパーメソッドであり、
+    # routes.rbの内容に基づいて自動的に生成される。
+    get static_pages_help_url
     assert_response :success
   end
 
-  test "should get help" do
-    get static_pages_help_url
+  test 'should get about' do
+    get static_pages_about_url
     assert_response :success
   end
 end
