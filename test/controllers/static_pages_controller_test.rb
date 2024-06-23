22ed49c922375ee8158da6df236fdf1591a6a03b
Koichiro Mikama <koichiro.mika@gmail.com>
2024-06-23 09:32:11 +0900

    Add setup to static_pages_controller test

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,11 +1,17 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  # setupは特殊なキーワード。
+  # テストフレームワークminitestにおいて、各テストメソッドが実行される直前に実行される特別なメソッドになる。
+  def setup
+    @base_title = 'Ruby on Rails Tutorial Sample App'
+  end
+
   test 'should get home' do # 「homeページのテスト」
     get static_pages_home_url # static_pages/home のURLにHTTP GETリクエストを送信します。
     # このリクエストは、static_pagesコントローラのhomeアクションを呼び出します。
     assert_response :success # その結果のレスポンスは成功（例: HTTPステータスコード200）であるべきです。
-    assert_select 'title', 'Home | Ruby on Rails Tutorial Sample App' # titleタグの中身が「Home | Ruby on Rails Tutorial Sample App」であることを確認します。
+    assert_select 'title', "Home | #{@base_title}" # titleタグの中身が「Home | Ruby on Rails Tutorial Sample App」であることを確認します。
   end
 
   test 'should get help' do
@@ -13,12 +19,12 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     # routes.rbの内容に基づいて自動的に生成される。
     get static_pages_help_url
     assert_response :success
-    assert_select 'title', 'Help | Ruby on Rails Tutorial Sample App'
+    assert_select 'title', "Help | #{@base_title}"
   end
 
   test 'should get about' do
     get static_pages_about_url
     assert_response :success
-    assert_select 'title', 'About | Ruby on Rails Tutorial Sample App'
+    assert_select 'title', "About | #{@base_title}"
   end
 end
