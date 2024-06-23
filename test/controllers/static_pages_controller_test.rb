35af415d74fd76708eea102e667576932f29aa21
Koichiro Mikama <koichiro.mika@gmail.com>
2024-06-23 23:37:26 +0900

    Modify routings and the associated test code

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -7,34 +7,27 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     @base_title = 'Ruby on Rails Tutorial Sample App'
   end
 
-  test 'should get root' do
-    get root_url
-    assert_response :success
-  end
-
   test 'should get home' do # 「homeページのテスト」
-    get static_pages_home_url # static_pages/home のURLにHTTP GETリクエストを送信します。
+    get root_path # static_pages/home のURLにHTTP GETリクエストを送信します。
     # このリクエストは、static_pagesコントローラのhomeアクションを呼び出します。
     assert_response :success # その結果のレスポンスは成功（例: HTTPステータスコード200）であるべきです。
     assert_select 'title', @base_title # titleタグの中身が「Home | Ruby on Rails Tutorial Sample App」であることを確認します。
   end
 
   test 'should get help' do
-    # "static_pages_help_url"はテスト用のヘルパーメソッドであり、
-    # routes.rbの内容に基づいて自動的に生成される。
-    get static_pages_help_url
+    get help_path
     assert_response :success
     assert_select 'title', "Help | #{@base_title}"
   end
 
   test 'should get about' do
-    get static_pages_about_url
+    get about_path
     assert_response :success
     assert_select 'title', "About | #{@base_title}"
   end
 
   test 'should get contact' do
-    get static_pages_contact_url
+    get contact_path
     assert_response :success
     assert_select 'title', "Contact | #{@base_title}"
   end
