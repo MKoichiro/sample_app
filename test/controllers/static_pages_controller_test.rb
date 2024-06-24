0657e1a53543593d04d14ee005faa28e6a410090
Koichiro Mikama <koichiro.mika@gmail.com>
2024-06-24 14:06:23 +0900

    Use full_title helper instead of variable in static_pages_controller_test.rb

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,34 +1,35 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  # setupは特殊なキーワード。
-  # テストフレームワークminitestにおいて、各テストメソッドが実行される直前に実行される特別なメソッドになる。
+  # setup は特殊なキーワード
+  # テストフレームワーク minitest において、各テストメソッドが実行される直前に実行される特別なメソッドになる。
   def setup
-    @base_title = 'Ruby on Rails Tutorial Sample App'
   end
 
-  test 'should get home' do # 「homeページのテスト」
-    get root_path # static_pages/home のURLにHTTP GETリクエストを送信します。
-    # このリクエストは、static_pagesコントローラのhomeアクションを呼び出します。
-    assert_response :success # その結果のレスポンスは成功（例: HTTPステータスコード200）であるべきです。
-    assert_select 'title', @base_title # titleタグの中身が「Home | Ruby on Rails Tutorial Sample App」であることを確認します。
-  end
-
-  test 'should get help' do
-    get help_path
+  test 'should get home' do
+    get root_path
     assert_response :success
-    assert_select 'title', "Help | #{@base_title}"
+    assert_select 'title', full_title
+  end
+
+  test 'should get help' do # 「help ページのテスト」
+    # TEST 1
+    get help_path            # 「"/help" の URL に HTTP GET リクエストを送信してください。」
+    assert_response :success # 「そのレスポンスは成功（HTTP ステータスコード 2XX）であるべきです。」
+
+    # TEST 2
+    assert_select 'title', full_title('Help') # 「title タグの中身は「Home | Ruby on Rails Tutorial Sample App」であるべきです。」
   end
 
   test 'should get about' do
     get about_path
     assert_response :success
-    assert_select 'title', "About | #{@base_title}"
+    assert_select 'title', full_title('About')
   end
 
   test 'should get contact' do
     get contact_path
     assert_response :success
-    assert_select 'title', "Contact | #{@base_title}"
+    assert_select 'title', full_title('Contact')
   end
 end
