65fb804992ac570da41c13e3ba2532b2ebbe7cb4
Koichiro Mikama <koichiro.mika@gmail.com>
2024-06-26 17:25:53 +0900

    feat: Add presence validation and tests against 'name' and 'email' for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,19 @@
 class User < ApplicationRecord
+  validates :name, presence: true
+  validates :email, presence: true
 end
+
+# memo 1:
+# - `validates :name, presence: true` = `validates(:name, { presence: true })`
+# - `presence: true` は、`name` が空文字列でないことを検証する。全角スペースにも有効。
+
+# memo 2:
+# `User` -> `ApplicationRecord` -> `ActiveRecord::Base` の継承関係
+# `Application::Base` で Active Record の主要な機能が提供されている。
+# 一般に、`A::B` という構文は `A` クラスまたはモジュールの中で定義されている `B` という名前空間を指す。
+# なお、Ruby で名前空間はクラスやモジュールによって形成される。
+#
+# `ApplicationRecord` がクラス、`ActiveRecord` がモジュール、`Base` がクラスであるので、
+# `ApplicationRecord < ActiveRecord::Base` という構文は、
+# 「`ApplicationRecord` クラスが、`ActiveRecord` モジュール内で定義されている `Base` クラスを参照・継承している」
+# ことを示している。
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,24 @@
-require "test_helper"
+require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(name: 'Example User', email: 'user.example.com')
+  end
+
+  # user は有効でなければならない
+  test 'should be valid' do
+    assert @user.valid?
+  end
+
+  # name は空文字列であってはならない
+  test 'name should be present' do
+    @user.name = '     '
+    assert_not @user.valid?
+  end
+
+  # email は空文字列であってはならない
+  test 'email should be present' do
+    @user.email = '     '
+    assert_not @user.valid?
+  end
 end
