ce3e299e3506b3b706ad651c05fe8cc77c5a9ce1
Koichiro Mikama <koichiro.mika@gmail.com>
2024-06-29 18:34:33 +0900

    feat: Redirect to 'users/:id' (page for user profile) and Show flash message on successful signup and Add associated test codes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,8 +11,13 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # Handle a successful save.
+      flash[:success] = 'Welcome to the Sample App!'
+      # GETリクエストを送信して別ページに遷移。
+      # 変数名に Convention は無く、中身が `User` クラスのオブジェクトであることが重要。
+      redirect_to @user
     else
+      # 新しいリクエスト無しで、単に再描画。
+      # 'new' は、`new.html.erb` を指す。
       render 'new', status: :unprocessable_entity
     end
   end
@@ -57,3 +62,15 @@ end
 # 現在のバージョンでは、この書き方はエラーになる。
 # 例えば、dev tool から input の name 属性を変更して、不正なデータを送信することができる。
 # より具体的には、like 属性を編集して「いいね」数の不正、admin 属性を付与して管理者権限の不正などが考えられる。
+
+# memo 4: `redirect_to @user`
+# `redirect_to user_url(@user)` と等価。
+# `user_url(@user)` は、`http://www.example.com/users/:id` にリダイレクトする。
+
+# memo 5: `flash` と `redirect_to`
+# `flash` はフラッシュメッセージの表示に使うところから名前が付いたのだろうが本質は、
+# リクエスト間で情報を保持するための仕組み。
+# 通常のインスタンス変数に格納した値は、ライフサイクルが終了すると消えるが、
+# `flash` に格納した値は、次のリクエストまで保持され、そのライフサイクルが終了時（レンダリング後）に消える。
+# よって、`flash` は `redirect_to` とセットで使用し、リダイレクト先で `flash` の値を用いてフラッシュメッセージを表示する
+# という実装ができる。
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
+  # 無効なユーザーデータではサインアップできてはならない
   test 'invalid signup information' do
     # 省略可能だが、実際のユーザーの手順通りにまずは '/signup' にアクセスする
     get signup_path
@@ -22,5 +23,28 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     # エラーメッセージが表示されていることを確認
     assert_select 'div#error_explanation'
     assert_select 'div.field_with_errors'
+  end
+
+  # 有効なユーザーデータではサインアップできるべき
+  test 'valid signup information' do
+    get signup_path
+    # ブロックの処理前後で User.count が 1 増えることを確認
+    assert_difference 'User.count', 1 do
+      post users_path, params: {
+        user: {
+          name: 'Example Taro',
+          email: 'taro@example.com',
+          password: 'tutorial',
+          password_confirmation: 'tutorial'
+        }
+      }
+
+      # リダイレクト先に移動
+      follow_redirect!
+      assert_template 'users/show'
+
+      # リダイレクト先でフラッシュメッセージが表示されていることを確認
+      assert_not flash.empty?
+    end
   end
 end
