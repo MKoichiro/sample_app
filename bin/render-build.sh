d3dc79b8698d264983d21870aa052df0b953d8e2
Koichiro Mikama <koichiro.mika@gmail.com>
2024-07-29 15:42:06 +0900

    feat: Add sample users to the database during deployment

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,4 +5,13 @@ set -o errexit
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
-bundle exec rails db:migrate
+# bundle exec rails db:migrate
+DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:migrate:reset
+
+# 事前に用意したサンプルユーザーをデータベースに追加
+bundle exec rails db:seed
+
+
+# memo 1: `DISABLE_DATABASE_ENVIRONMENT_CHECK=1`
+# Railsのデータベース操作に関する安全チェックを無効化し、
+# デプロイ時にデータベースのリセット(`bundle exec rails db:migrate:reset`)を可能に。
